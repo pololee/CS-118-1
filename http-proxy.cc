598a18f9465f283e2e50ef5731b10fb6b560ab4c
pololee <pololee1990@gmail.com>
2013-04-27 00:05:44 -0700

    add http-common

--- http-proxy.cc
+++ http-proxy.cc
@@ -1,12 +1,19 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/** @file http-proxy.cc
+*@author Xiao Li (pololee@cs.ucla.edu)
+*@data 2013/4/26
+*/
 
 #include <iostream>
-
+#include "http-common.h"
 using namespace std;
 
 int main (int argc, char *argv[])
 {
   // command line parsing
-  cout<<"hello, world!"<<endl;  
+  cout<<"hello, world!"<<endl;
+  int proxySockFD = iniServerListen(PROXY_SERVER_PORT);
+	cout<<"finish listen\n";
+	cout<<"proxySockFD: "<<proxySockFD<<endl;
   return 0;
 }
